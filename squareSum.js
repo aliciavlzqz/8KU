e657aaec6856381a196234a7938ff0a5d5cb5b28
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-03-12 08:08:13 -0600

    Add alternate codewars solution.

--- squareSum.js
+++ squareSum.js
@@ -12,3 +12,14 @@ for(let i = 0; i < numbers.length; i++){
 }
   return sum
 }
+
+
+
+
+///alternate solution
+//arr.reduce() method in JavaScript is used to reduce the array to a single value and executes a provided function for each 
+//value of the array (from left-to-right) and the return value of the function is stored in an accumulator. 
+
+function squareSum(numbers){
+  return numbers.reduce((sum,num) => sum + (num * num), 0);
+}
