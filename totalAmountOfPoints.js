927c01b5f214f6d5d442065bd39eacc20b9baf9b
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-06 09:48:57 -0500

    add 8ku codewars challenge instructions

---
+++ totalAmountOfPoints.js
@@ -0,0 +1,4 @@
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string
+//  in the format "x:y", where x is our team's score and y is our opponents score.
