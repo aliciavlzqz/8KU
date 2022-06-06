f8ab955a3a5a7e251bfa6c9677d863f10ff8427d
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-06 09:51:22 -0500

    add additional instructions for total amount of points 8ku challenge.

--- totalAmountOfPoints.js
+++ totalAmountOfPoints.js
@@ -2,3 +2,11 @@
 
 // Our team's match results are recorded in a collection of strings. Each match is represented by a string
 //  in the format "x:y", where x is our team's score and y is our opponents score.
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
