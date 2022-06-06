e83b7394a6b277581780aea43225a75778cafd08
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-06 10:00:26 -0500

    add total amount code wars challenge answer.

--- totalAmountOfPoints.js
+++ totalAmountOfPoints.js
@@ -10,3 +10,17 @@
 // our team always plays 10 matches in the championship
 // 0 <= x <= 4
 // 0 <= y <= 4
+
+
+function points(games) {
+ 
+    let sum = 0
+    for(let i =0; i < games.length; i ++){
+      //add conditional
+      if(games[i][0] > games[i][2])
+        sum += 3;
+      if(games[i][0] == games[i][2])
+        sum += 1
+    }
+    return sum
+  }
