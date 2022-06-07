bdb82e854f2571a87d6c4044991f9bc63a8d639a
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-07 10:09:20 -0500

    add alternate solution for totalAmount of Points 8ku codewars challenge.

--- totalAmountOfPoints.js
+++ totalAmountOfPoints.js
@@ -24,3 +24,17 @@ function points(games) {
     }
     return sum
   }
+
+//alternate solution
+
+  function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }
