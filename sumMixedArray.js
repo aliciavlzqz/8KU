d85bda79ea80ba3d41101232585f1862937216e4
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-08 11:38:12 -0500

    add 8ku codewars solution for sum of mixed array.

--- sumMixedArray.js
+++ sumMixedArray.js
@@ -1,3 +1,12 @@
 // Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
 // Return your answer as a number.
+
+
+function sumMix(x){
+    var somme = 0;
+    for (i = 0; i<x.length; i++) {
+      somme += Number(x[i]);
+    }
+    return somme;
+  }
