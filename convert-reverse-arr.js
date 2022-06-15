ca5ff8e08457115e70f05805517ccb8016470025
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-15 09:32:09 -0500

    add codewars 8ku solution

--- convert-reverse-arr.js
+++ convert-reverse-arr.js
@@ -1,5 +1,11 @@
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
 function digitize(n) {
-
+ //code here\
+  //convert to string
+  //.split divides string into substrings, puts substring into an array and returns the array
+  //.reverse reverses an array
+  //.map creates a new array from calling a function for every array element
+  //Number converts a value into a number
+  return String(n).split('').reverse().map(Number)
 }
