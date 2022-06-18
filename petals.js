479c7c31cb596db465f070fa60e928fa99bd0c3d
Martha Martinez <105942606+aliciavlzqz@users.noreply.github.com>
2022-06-18 13:27:33 -0500

    Add alternate solution to codewars 8ku array challenge

--- petals.js
+++ petals.js
@@ -18,3 +18,18 @@ function howMuchILoveYou(nbPetals) {
     var index = (nbPetals - 1) % petalSaying.length;
     return petalSaying[index];
 }
+
+
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+     return phrases[(n - 1) % phrases.length] 
+}
